2f458ea45d267c1af295d5c669f3305c824f534b
Al Mamun <wdmamun75@gmail.com>
2023-10-04 11:01:59 +0600

    added login form from daisy ui

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,35 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="text-center">
+                <h1>this is login page</h1>
+                <form className="md:w-3/4 lg:w-2/4 mx-auto">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
